6c445a2a5fd8ec463767027573881b0af11ffbf1
Or Elmaliah <or.e@walkme.com>
2018-05-02 18:57:32 +0300

    Version 1.7.0

--- include/ABBISDK/WMUserData.h
+++ include/ABBISDK/WMUserData.h
@@ -11,78 +11,88 @@
 @interface WMUserData : NSObject
 
 /**
+ * User Attributes in current session.
+ */
+@property (nonatomic,strong) NSDictionary *userAttributes;
+
+/**
+ * Private User Attributes in current session.
+ */
+@property (nonatomic,strong) NSDictionary *privateUserAttributes;
+
+/**
  * User Session Duration in second.
  */
-@property (nonatomic,strong)  NSNumber* sessionDuration;
+@property (nonatomic,strong) NSNumber *sessionDuration;
 
 /**
  * User's iOS version
  */
-@property (nonatomic,strong)  NSString* systemVersion;
+@property (nonatomic,strong) NSString *systemVersion;
 
 /**
  * Device Unique ID
  */
-@property (nonatomic,strong)  NSString* deviceId;
+@property (nonatomic,strong) NSString *deviceId;
 
 /**
  * Device Model.
  */
-@property (nonatomic,strong)  NSString* deviceModel;
+@property (nonatomic,strong) NSString *deviceModel;
 
 /**
- * Device Orientation PRT_REG / LSL.
+ * Device Orientation PRT_REG / PRT_USD / LSL / LSR.
  */
-@property (nonatomic,strong)  NSString* deviceOrientation;
+@property (nonatomic,strong) NSString *deviceOrientation;
 
 /**
  * The application version.
  */
-@property (nonatomic,strong)  NSString* appVersion;
+@property (nonatomic,strong) NSString *appVersion;
 
 /**
  * The application name.
  */
-@property (nonatomic,strong)  NSString* appName;
+@property (nonatomic,strong) NSString *appName;
 
 /**
  * Locale language for user's device.
  */
-@property (nonatomic,strong)  NSString* locale;
+@property (nonatomic,strong) NSString *locale;
 
 /**
  * WalkMe SDK version.
  */
-@property (nonatomic,strong)  NSString* sdkVer;
+@property (nonatomic,strong) NSString *sdkVer;
 
 /**
  * Session unique id.
  */
-@property (nonatomic,strong)  NSString* sessionId;
+@property (nonatomic,strong) NSString *sessionId;
 
 /**
  * If Push notification approved for user.
  */
-@property (nonatomic,strong)  NSString* isPushEnabled;
+@property (nonatomic,strong) NSString *isPushEnabled;
 
 /**
  * User device timezone.
  */
-@property (nonatomic,strong)  NSString* timezone;
+@property (nonatomic,strong) NSString *timezone;
 
 /**
  * User current network ( WIFI / 3G ).
  */
-@property (nonatomic,strong)  NSString* network;
+@property (nonatomic,strong) NSString *network;
 
 /**
  * User OS System Name.
  */
-@property (nonatomic,strong)  NSString* systemName;
+@property (nonatomic,strong) NSString *systemName;
 
 /**
  * Current Timestamp for this User.
  */
-@property (nonatomic,strong)  NSString* timestamp;
+@property (nonatomic,strong) NSString *timestamp;
 
 @end
