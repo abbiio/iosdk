521b9e499e9d6cb7173ee848e58af83230e31a44
ec2-user <ec2-user@ip-10-174-6-53.eu-west-1.compute.internal>
2021-09-12 07:09:12 +0000

    Version 2.12.0

--- Package.swift
+++ Package.swift
@@ -8,6 +8,6 @@ let package = Package(
                       targets: ["WalkMeSDK"])],
   targets: [.binaryTarget(name: "WalkMeSDK",
                           url: "https://assets.abbi.io/release/sdk/2.12.0.xcframework.zip",
-                          checksum: "142f4df3ecfb38753a7e9696e152cf15338b7fb463cfe92420c0f670cc6091fa")],
+                          checksum: "d2b98bbea86dc8436925c8cde58fd64f83a5daa9767e3d6a87ddaf28bd8a97ce")],
   swiftLanguageVersions: [.v5]
 )
